8082b45499e87e088fcfb877fb899b309013b5f3
kamelbataineh <batainehkamel2@gmail.com>
2024-06-25 13:56:50 +0300

    Update project01.dart

--- task1/project01.dart
+++ task1/project01.dart
@@ -1,23 +1,9 @@
 import 'dart:io';
 
 void main() {
-  String name="kamel   ";
-  print(name.length);
-  
-
-
-
-  // print(
-  //     " WElcom to our company \n Please Enter your username aad password to login :");
-
-  // stdout.write("username:");
-  // String username = stdin.readLineSync()!;
-
-  // stdout.write("password:");
-  // String ? password = stdin.readLineSync() ;
-
-  // stdout.write("Enter your age:");
-  // int age = int.parse(stdin.readLineSync()!);
-
-  // print("username: $username \npassword: $password \nage: $age ");
+  print("Hello, in this code we will using \"Escape characters\"");
+  print(
+      "This is backslash \\\nThis is double quotes \"\nThis is single quotes ',");
+  print(
+      "we can print new line using \" \\n \" \nand print spaces using \" \\t \"");
 }
